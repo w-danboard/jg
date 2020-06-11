de5fb16111ebf441bebee142b46ed4183b782882
wanglin <wanglin46@lenovo.com>
2020-06-11 13:54:22 +0800

    aa

---
+++ 单元测试/mocha-vue/src/components/GetData.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{user}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+
+@Component
+export default class GetData extends Vue {
+  user: string = 'wl';
+
+  mounted () {
+    axios.get('/user').then(res => {
+      this.user = res.data.user
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+}
+</script>
---
+++ 单元测试/mocha-vue/tests/unit/GetData.spec.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import { expect } from 'chai';
+import GetData from '@/components/GetData.vue'
+import { mount } from '@vue/test-utils';
+
+// mocha + axios 获取数据 但数据不存在
+// mocha + axios = moxios
+describe('测试异步获取数据', () => {
+  it('', () => {})
+})
