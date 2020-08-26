da2612cd841bae1ffb054ab23c6ad39c1f833754
wanglin <wanglin46@lenovo.com>
2020-08-26 16:12:41 +0800

    c

--- webpack/2020nian02qizaixiankuangjiake/webpack02/src/other.js
+++ webpack/2020nian02qizaixiankuangjiake/webpack02/src/other.js
@@ -1,2 +1,3 @@
 import { sum } from './a' 
+import './css/1.less'
 console.log(sum(1000, 1))
--- webpack/2020nian02qizaixiankuangjiake/webpack02/webpack.config.js
+++ webpack/2020nian02qizaixiankuangjiake/webpack02/webpack.config.js
@@ -1,6 +1,7 @@
 const path  = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const ary = ['index', 'other']
 function htmlP (ary = []) {
@@ -38,7 +39,10 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    ...htmlP(ary)
+    ...htmlP(ary),
+    new MiniCssExtractPlugin({
+      filename: 'css/main.[hash:5].css'
+    })
     // new HtmlWebpackPlugin({
     //   template: './public/index.html',
     //   filename: 'index.html',
@@ -55,11 +59,43 @@ module.exports = {
   module: {
     rules: [
       // 存放了各种loader
+      // 工作中 我们的CSS一般都是单独合拼出一个css文件的 需要使用 mini-css-extract-plugin
       {
         test: /\.css$/, // 匹配哪些文件 （加载CSS文件 至少需要css-loader 和style-loader）
         // 先用css-loader加载CSS文件 然后用style-loader扔到页面上
         // loader的加载顺序是相反的 是从右向左的
-        use: ['style-loader', 'css-loader']   // 使用什么loader 1、直接写字符串(适用于只需要一个loader) 2、数组 多个loader 3、{需要单独配置一个属性时}
+        use: [{
+          loader: MiniCssExtractPlugin.loader
+        }, {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 2 // 先去使用后边的加载器加载
+          }
+        }, 'postcss-loader', 'less-loader']   // 使用什么loader 1、直接写字符串(适用于只需要一个loader) 2、数组 多个loader 3、{需要单独配置一个属性时}
+      },
+      // 我们写一些css高级属性时 比如css3 想要兼容不同的浏览器 需要使用postcss-loader这个加载器
+      // 使用postcss-loader 需要配置postcss.config.js这个配置文件
+      // 还需要告诉webpack要兼容那些浏览器 .browserslistrc 写入cover 99.9% 或则如下：
+      /**
+       * 在package.json中配置
+       * browserslist: {
+       *  production: [
+       *    >0.2%,
+       *    not dead,
+       *    not op_mini all
+       *  ],
+       *  development: [
+       *    last 1 chrome version,
+       *    last 1 firefox version,
+       *    last 1 safari version
+       *  ]
+       * }
+       */
+      {
+        test: /\.less$/,
+        // less首先要编译成css 还需要走css编译的那套
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'],
+        exclude: '/node_modules/' // 不会去加载node_modules中的less文件 也就是不会去编译
       }
     ]
   },
